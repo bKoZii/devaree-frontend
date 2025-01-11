a0af78428e92f212d6066f6075fd1ceb6dd09812
bKoZii <bigkonkamon@gmail.com>
2025-01-11 14:34:01 +0700

    feat(Footer): :lipstick: Dark mode BG Color

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <footer class="body-font bg-primary-100 text-gray-600">
+    <footer class="body-font bg-primary-100 dark:bg-primary-950 text-gray-600 dark:text-white">
       <div class="container mx-auto flex flex-col items-center px-5 py-8 sm:flex-row">
-        <a class="title-font flex items-center justify-center font-medium text-gray-900 md:justify-start">
+        <a class="flex items-center justify-center font-bold md:justify-start">
           <span class="ml-3 text-xl">ดีวารี @ สปา</span>
         </a>
-        <p class="mt-4 text-sm text-gray-500 sm:ml-4 sm:mt-0 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:pl-4">
+        <p class="mt-4 text-sm text-gray-500 sm:ml-4 sm:mt-0 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:pl-4 dark:text-gray-400">
           © 2025  — กรกมล ศรีอ่อน
         </p>
         <span class="mt-4 inline-flex justify-center space-x-3 sm:ml-auto sm:mt-0 sm:justify-start">
